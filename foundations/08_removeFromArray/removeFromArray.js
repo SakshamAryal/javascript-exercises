a2dc1ef8ebff63e221568fd1d5c00dfacebba91c
SakshamAryal <163455753+SakshamAryal@users.noreply.github.com>
2025-12-27 21:59:17 -0500

    done in fewer lines

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(list) {
+    let arr = Array.prototype.slice.call(arguments, 1);
+    return list.filter(a => !arr.includes(a));
 };
 
 // Do not edit below this line
