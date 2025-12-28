4de80c736f45e4dbd6d351c0d7a93286e409a19e
SakshamAryal <163455753+SakshamAryal@users.noreply.github.com>
2025-12-27 21:36:09 -0500

    done solved

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let output = "";
+    let i = word.length- 1;
+    while (i >= 0){
+        output += word[i];
+        i -= 1;
+    }
+    return output
 };
 
 // Do not edit below this line
