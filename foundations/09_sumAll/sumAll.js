85b473c16a23e063993de69437130d6358710aa4
SakshamAryal <163455753+SakshamAryal@users.noreply.github.com>
2025-12-27 22:13:18 -0500

    too many comparisions

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let output = 0;
+    if (a < 0 || b < 0 || typeof(a) != "number" || typeof(b) != "number" || a%1 != 0 || b%1 != 0){
+        return "ERROR";
+    }
+    for (let i = Math.min(a,b); i <= Math.max(a,b); i++){
+        output += i;
+    }
+    return output
 };
 
 // Do not edit below this line
