85d2742d5d30f377b7d090932a9415e3ff6bbfb5
SakshamAryal <163455753+SakshamAryal@users.noreply.github.com>
2026-04-02 19:08:15 -0400

    all test cases passed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let a = str.split("");
+    let length = a.length/2;
+    let i = 0;
+    while(i <= length){
+        if (a[i] != a[length*2-i-1]) {
+            return false;
+        };
+        i += 1
+    };
+    return true;
 };
 
 // Do not edit below this line
