a26add62e43fa58239c16df42309908a126e499e
SakshamAryal <163455753+SakshamAryal@users.noreply.github.com>
2026-04-03 14:38:24 -0400

    took me a while to get it

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(list) {
+    let oldest = list[0];
+    let oldestAge = oldest.yearOfDeath
+        ? oldest.yearOfDeath - oldest.yearOfBirth
+        : new Date().getFullYear() - oldest.yearOfBirth;
 
+    for (const person of list.slice(1)) {
+        const age = person.yearOfDeath
+            ? person.yearOfDeath - person.yearOfBirth
+            : new Date().getFullYear() - person.yearOfBirth;
+
+        if (age > oldestAge) {
+            oldest = person;
+            oldestAge = age;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
